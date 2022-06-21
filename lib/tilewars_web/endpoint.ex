a810877e1ad6746830fcd64ea916d27a17d9424a
Roman Sierkov <roman@sierkov.dev>
2022-06-21 16:41:31 +0300

    Basic phoenix socket setup

--- lib/tilewars_web/endpoint.ex
+++ lib/tilewars_web/endpoint.ex
@@ -10,6 +10,10 @@ defmodule TilewarsWeb.Endpoint do
     signing_salt: "CRTuZ8MY"
   ]
 
+  socket "/ws", TilewarsWeb.GameSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
